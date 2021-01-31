212a1b7196495ba2d52ee160c0859f8c8c9878f3
vardan98 <vardanpogosyan98@gmail.com>
2021-02-01 00:36:52 +0400

    configs

--- vendor/config/config.php
+++ vendor/config/config.php
@@ -1,7 +1,7 @@
 <?php
-define('BASEURL', "http://" . $_SERVER['SERVER_NAME'].'/');
+define('BASEURL', "https://" . $_SERVER['SERVER_NAME'].'/');
 
-define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', '');
-define('DB_NAME', 'mvc');
+define('DB_HOST', 'eu-cdbr-west-03.cleardb.net');
+define('DB_USER', 'be0ca52a6e98a9');
+define('DB_PASS', 'c72a4d54');
+define('DB_NAME', 'heroku_d541add4d2739a8');
